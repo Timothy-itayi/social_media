2822ed36c93a5852a52a4177bf1e5861d8695055
Timothy <timmytam10@gmail.com>
2025-05-31 15:28:41 +1000

    set up backend with nodejs

---
+++ slip_stream_backend/controllers/postController.js
@@ -0,0 +1,13 @@
+const pool = require('../db/db');
+
+const getAllPosts = async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM posts');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: 'Server error' });
+  }
+};
+
+module.exports = { getAllPosts };
